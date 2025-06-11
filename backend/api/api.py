ed6ead78fa1aeed205ed24094ec9f61442f209b3
thenikolayli <nikolay.li2008@gmail.com>
2025-06-11 00:00:57 -0700

    worked the recursive function, need to add saving capabilities

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,17 +1,27 @@
-import base64, smtplib, json
+import smtplib
 from email.message import EmailMessage
 
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
+
 from .models import Message
 from os import getenv
 from dotenv import load_dotenv
+
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
 
 load_dotenv()
 
 router = APIRouter(prefix="", tags=["api"])
 keyclub_email = getenv("KEYCLUB_EMAIL")
 app_password = getenv("APP_PASSWORD")
+
+SCOPES = getenv("API_SCOPES")
+credentials = Credentials.from_service_account_file("key.json", scopes=SCOPES)
+service = build("drive", "v3", credentials=credentials)
+
+
 
 @router.post("/message")
 async def message(message: Message):
