f56ffa0c91d812da212c3f115cce05584f69c050
thenikolayli <nikolay.li2008@gmail.com>
2025-07-15 03:50:43 -0700

    added logging to the api

--- backend/api/api.py
+++ backend/api/api.py
@@ -4,11 +4,14 @@ from email.message import EmailMessage
 from pathlib import Path
 
 from fastapi import APIRouter, status
-from fastapi.responses import JSONResponse, Response
+from fastapi.responses import JSONResponse
 
-from .models import Message
+from backend.api.models import Message, EventLoggedRequestModel, MeetingLoggedRequestModel
+from backend.api.keyclubutils import log_event, log_meeting
 from os import getenv
 from dotenv import load_dotenv
+from datetime import datetime
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
@@ -19,14 +22,38 @@ router = APIRouter(prefix="", tags=["api"])
 keyclub_email = getenv("KEYCLUB_EMAIL")
 app_password = getenv("APP_PASSWORD")
 
-SCOPES = json.loads(getenv("API_SCOPES"))
-PHOTOS_FOLDER_ID = getenv("PHOTOS_FOLDER_ID")
+scopes = json.loads(getenv("API_SCOPES"))
+photos_folder_id = getenv("PHOTOS_FOLDER_ID")
 keyjson_path = str((Path(__file__).parent / "../key.json").resolve())
-credentials = Credentials.from_service_account_file(keyjson_path, scopes=SCOPES)
-service = build("drive", "v3", credentials=credentials)
 folder_mimeType = 'application/vnd.google-apps.folder'
 image_mimeTypes = ["image/jpeg", "image/png", "image/webp", "image/heif"]
+api_key = getenv("API_KEY")
 
+credentials = Credentials.from_service_account_file(keyjson_path, scopes=scopes)
+service = build("drive", "v3", credentials=credentials)
+sheets_service = build("sheets", "v4", credentials=credentials)
+docs_service = build("docs", "v1", credentials=credentials)
+
+
+# ------------------EMAIL API STUFF------------------
+
+# sends a message to the gmail
+@router.post("/message")
+async def message(email_message: Message):
+    email = EmailMessage()
+    email["To"] = keyclub_email
+    email["From"] = keyclub_email
+    email["Subject"] = f"Website Message from {email_message.first_name} {email_message.last_name}"
+    email.set_content(f"Contact: {email_message.contact}\n\n{email_message.message}")
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(keyclub_email, app_password)
+        smtp.send_message(email)
+
+    return JSONResponse("Message Sent", status_code=status.HTTP_200_OK)
+
+
+# ------------------PHOTOS (WEBSITE GALLERY) API STUFF------------------
 
 def get_photos_recursive(folder_id):
     image_file_ids = []
@@ -46,26 +73,10 @@ def get_photos_recursive(folder_id):
 def download_photo(url, index):
     urllib.request.urlretrieve(url, f"photos/photo_{index}.png")
 
-
-# sends a message to the gmail
-@router.post("/message")
-async def message(message: Message):
-    email = EmailMessage()
-    email["To"] = keyclub_email
-    email["From"] = keyclub_email
-    email["Subject"] = f"Website Message from {message.first_name} {message.last_name}"
-    email.set_content(f"Contact: {message.contact}\n\n{message.message}")
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(keyclub_email, app_password)
-        smtp.send_message(email)
-
-    return JSONResponse("Message Sent", status_code=status.HTTP_200_OK)
-
 # downloads all photos from the google drive
 @router.get("/update_photos")
 async def update_photos():
-    global PHOTOS_FOLDER_ID
+    global photos_folder_id
 
     # deletes old photos
     for file in os.listdir("photos"):
@@ -73,7 +84,7 @@ async def update_photos():
         os.remove(file_path)
 
     # downloads new photos
-    photo_urls = get_photos_recursive(PHOTOS_FOLDER_ID)
+    photo_urls = get_photos_recursive(photos_folder_id)
     with ThreadPoolExecutor(max_workers=10) as executor:
         futures = [executor.submit(download_photo, photo_urls[index], index) for index in range(len(photo_urls))]
 
@@ -95,3 +106,88 @@ async def get_photos(count: int = 20):
         file_list.remove(choice)
 
     return photo_list
+
+
+# ------------------EVENT LOGGING API STUFF------------------
+
+# gets a collection from the database (look up how mongodb stores files and how their filesystem works)
+async def get_collection(collection_name):
+    client = AsyncIOMotorClient(getenv("MONGO_URI"))
+    db = client["main"] # theres only one database
+    collection = db[collection_name]
+
+    return collection
+
+# saves an event or meeting to the database
+async def save_event_to_db(response):
+    # creates db entry
+    title = response.get("event_title")
+    hours_logged = 0
+    hours_not_logged = 0
+    people_attended = 0
+
+    # volunteers logged
+    if response.get("logged"):
+        for volunteer_logged, data in response.get("logged").items():
+            hours_logged += data
+            people_attended += 1
+
+    # volunteers not logged
+    if response.get("not_logged"):
+        for volunteer_not_logged, data in response.get("not_logged").items():
+            hours_not_logged += data
+            people_attended += 1
+
+    events_logged_collection = await get_collection("events_logged")
+    await events_logged_collection.insert_one({
+        "timestamp": datetime.now(),
+        "title": title,
+        "hours_logged": hours_logged,
+        "hours_not_logged": hours_not_logged,
+        "people_attended": people_attended,
+    })
+
+@router.post("/log_event")
+async def keyclub_log_event(event_data: EventLoggedRequestModel):
+    if event_data.api_key != api_key:
+        return JSONResponse("invalid api key", status_code=status.HTTP_401_UNAUTHORIZED)
+
+    document_id = event_data.link
+    hours_multiplier = event_data.hours_multiplier
+    log_event_response = log_event(
+        document_id=document_id,
+        hours_multiplier=hours_multiplier,
+        docs_service=docs_service,
+        sheets_service=sheets_service
+    )
+
+    if log_event_response.get("error"):
+        return JSONResponse(log_event_response.get("error"), status_code=status.HTTP_400_BAD_REQUEST)
+
+    await save_event_to_db(log_event_response)
+    return JSONResponse(log_event_response, status_code=status.HTTP_200_OK)
+
+@router.post("/log_meeting")
+async def keyclub_log_meeting(meeting_data: MeetingLoggedRequestModel):
+    if meeting_data.api_key != api_key:
+        return JSONResponse("invalid api key", status_code=status.HTTP_401_UNAUTHORIZED)
+
+    document_id = meeting_data.link
+    first_name_col = meeting_data.first_name_col
+    last_name_col = meeting_data.last_name_col
+    meeting_length = meeting_data.meeting_length
+    meeting_title = meeting_data.title
+    log_meeting_response = log_meeting(
+        document_id=document_id,
+        first_name_col=first_name_col,
+        last_name_col=last_name_col,
+        meeting_length=meeting_length,
+        meeting_title=meeting_title,
+        sheets_service=sheets_service
+    )
+
+    if log_meeting_response.get("error"):
+        return JSONResponse(log_meeting_response.get("error"), status_code=status.HTTP_400_BAD_REQUEST)
+
+    await save_event_to_db(log_meeting_response)
+    return JSONResponse(log_meeting_response, status_code=status.HTTP_200_OK)
--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from os import getenv
-from api.api import router as api_router
-import json
+from backend.api.api import router as api_router
 
 load_dotenv()
-app = FastAPI(docs_url="/api", redoc_url=None, openapi_url="/openapi.json", root_path="/api", host="0.0.0.0", port=8000)
+api_url = getenv('API_URL')
+app = FastAPI(docs_url=None, redoc_url=api_url, openapi_url="/openapi.json", root_path="/api", host="0.0.0.0", port=8000)
 app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=json.loads(getenv("CORS_ORIGINS")),
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
